b47bcef147d44691d0f82159a92c635298871a5b
jyR <kfmxyqtycn1@gmail.com>
2023-02-15 21:18:00 +0900

    feat: react 변경 작업중

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,40 @@
+import * as React from 'react';
+import Pagination from '@mui/material/Pagination';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function Main() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
+        <InputLabel id="aa">Age</InputLabel>
+        <Select
+          labelId="aa"
+          id="aa"
+          value={age}
+          onChange={handleChange}
+          autoWidth
+          label="Age"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Twenty</MenuItem>
+          <MenuItem value={21}>Twenty one</MenuItem>
+          <MenuItem value={22}>Twenty one and a half</MenuItem>
+        </Select>
+      </FormControl>
 
 
-
-
-function Main(){
-    return(
-        <main>
-        <section>
-            <h1>휴가신청서</h1>
-
-            <p>기안문서번호</p>
-            <p>기안 일시</p>
-             
-        </section>
-    </main>
-    );
+      <Pagination count={100} shape="rounded" />
+    </div>
+  );
 }
-
-export default Main
